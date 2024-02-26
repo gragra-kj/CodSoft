4af20151145fbe1670701c6f72e90c3926770a2b
Grace Muthui <muthuigracia27@gmail.com>
2024-02-26 12:53:05 +0300

    function for calculating( choices to select from)

--- calculator.py
+++ calculator.py
@@ -14,3 +14,12 @@ def divide(x,y):
 
 def calculator():
     print("Simple calculator")
+    first_num=float(input("Enter the first number: "))
+    sec_num=float(input("Enter the second number: "))
+
+    print("\nSelect your operation of choice ")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    
